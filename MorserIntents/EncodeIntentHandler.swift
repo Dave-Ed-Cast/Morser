da4e7453d74a0b20ede6fe769d6d9ab08204a2ca
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-06-02 12:30:38 +0200

    New feature:
    - Added the backbone for Intents, still incomplete

---
+++ MorserIntents/EncodeIntentHandler.swift
@@ -0,0 +1,16 @@
+//
+//  EncodeIntentHandler.swift
+//  Morser
+//
+//  Created by Davide Castaldi on 02/06/24.
+//
+
+import Foundation
+import Intents
+
+class EncodeIntentHandler: NSObject, EncodeIntentHandling {
+    func handle(intent: EncodeIntent, completion: @escaping (EncodeIntentResponse) -> Void) {
+        // Handle the intent here
+        
+    }
+}
